1be5da361372355f4f82e6c05d5417c3f769607a
filipe-mariz <57136731+filipe-mariz@users.noreply.github.com>
2021-02-23 16:22:40 -0300

    Funcionalidades prontas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ToDo';
+  public tarefa = "";
+  public items = ['item 1', 'item 2', 'item 3', 'item 4'];
+  addTarefa() {
+    this.items.push(this.tarefa)
+  }
+  removeTarefa(item: string) {
+    this.items.splice(this.items.indexOf(item), 1)
+  }
 }
